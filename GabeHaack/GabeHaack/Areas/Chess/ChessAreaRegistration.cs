93240c280a52f847931494951ad9356deccb1e96
Gabe Haack <gwhaack@gmail.com>
2015-08-22 10:14:16 -0700

    Initial MVC, couple modifications, Chess area

---
+++ GabeHaack/GabeHaack/Areas/Chess/ChessAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace GabeHaack.Areas.Chess
+{
+    public class ChessAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Chess";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Chess_default",
+                "Chess/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ GabeHaack/GabeHaack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GabeHaack.MvcApplication" Language="C#" %>
