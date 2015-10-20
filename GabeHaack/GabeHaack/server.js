ff2e291317937fa2122a21c1cce5ff8b8e845084
Gabe Haack <gwhaack@gmail.com>
2015-10-19 21:27:53 -0700

    New project

---
+++ GabeHaack/GabeHaack/server.js
@@ -0,0 +1,14 @@
+﻿var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = process.env.port || 1337;
+
+var app = express();
+app.use(bodyParser());
+
+app.get('/', function (requrest, response) {
+    response.send("Hello, I am Gabe.")
+});
+
+app.listen(port);
