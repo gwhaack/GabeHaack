03753ef419f9f9c9f1288d2df358c6c749d48621
Gabe Haack <gwhaack@gmail.com>
2015-10-19 21:06:49 -0700

    Removing chess area

---
+++ GabeHaack/GabeHaack/Controllers/ChessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GabeHaack.Controllers
+{
+    public class ChessController : Controller
+    {
+        // GET: Chess
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
